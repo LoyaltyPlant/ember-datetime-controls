bd839cd61dff7a3fdb0de8de071f6ff2236dea47
Eugene Gordeev <gordeev@loyaltyplant.com>
2017-01-19 03:09:26 +0300

    Fixed fonts importing to project && added updated icons from iconfonts

--- index.js
+++ index.js
@@ -5,5 +5,16 @@ module.exports = {
   name: 'ember-datetime-controls',
   isDevelopingAddon: function() {
     return true;
+  },
+
+  included: function (app) {
+    this._super.included.apply(this, arguments);
+
+    app.import('vendor/fonts/dt.svg', {destDir: 'assets/fonts'});
+    app.import('vendor/fonts/dt.eot', {destDir: 'assets/fonts'});
+    app.import('vendor/fonts/dt.ttf', {destDir: 'assets/fonts'});
+    app.import('vendor/fonts/dt.woff', {destDir: 'assets/fonts'});
+    app.import('vendor/fonts/dt.woff2', {destDir: 'assets/fonts'});
   }
+
 };
