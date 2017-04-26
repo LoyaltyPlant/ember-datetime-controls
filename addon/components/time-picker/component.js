2a3e0d8c9a62710c842a700ab130f39b23ce19c7
Eugene Gordeev <gordeev@loyaltyplant.com>
2017-04-26 12:09:01 +0300

    Fixed bug with time-control styling and added proper passed attributes handling

--- addon/components/time-picker/component.js
+++ addon/components/time-picker/component.js
@@ -1,13 +1,30 @@
-import Ember from "ember";
-import layout from "./template";
-import BasePickerMixin from "ember-datetime-controls/mixins/base-picker-mixin";
+import Ember from 'ember';
+import layout from './template';
+import BasePickerMixin from 'ember-datetime-controls/mixins/base-picker-mixin';
 
 
 const {
+  get,
+  inject: {
+    service
+  },
   Component,
 } = Ember;
 
 export default Component.extend(BasePickerMixin, {
   layout,
-  classNames: ['dt-pickers']
+  classNames: ['dt-pickers'],
+
+  moment: service(),
+
+  init() {
+    this._super(...arguments);
+
+    const moment = get(this, 'moment');
+
+    if (this.attrs.date && !this.attrs.time) {
+      this.time = moment.moment(this.attrs.date).format("HH:mm");
+    }
+  }
+
 });
